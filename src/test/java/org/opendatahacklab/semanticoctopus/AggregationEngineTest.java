87e9403b2225c90753c8bcc20b8b5bb6c691b4ed
cristianolongo <cristianolongo@herbrand>
2016-08-12 18:05:50 +0200

    simple test for loading no ontologies

--- src/test/java/org/opendatahacklab/semanticoctopus/AggregationEngineTest.java
+++ src/test/java/org/opendatahacklab/semanticoctopus/AggregationEngineTest.java
@@ -3,20 +3,48 @@
  */
 package org.opendatahacklab.semanticoctopus;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.jena.query.ResultSet;
 import org.junit.Test;
-import org.opendatahacklab.semanticoctopus.AggregationEngine;
 
 /**
  * Test cases for the {@link AggregationEngine}
+ * 
  * @author Cristiano Longo
  *
  */
 public class AggregationEngineTest {
 
+	public final URL ontologyA;
+	public final URL ontologyB;
+	public final URL ontologyC;
+
+	/**
+	 * @throws MalformedURLException
+	 * 
+	 */
+	public AggregationEngineTest() throws MalformedURLException {
+		ontologyA = new URL("http://opendatahacklab.github.io/semanticoctopus/testbed/A.owl");
+		ontologyB = new URL("http://opendatahacklab.github.io/semanticoctopus/testbed/B.owl");
+		ontologyC = new URL("http://opendatahacklab.github.io/semanticoctopus/testbed/C.owl");
+	}
+
+	/**
+	 * Check that if not ontology is provided as argument, the resulting
+	 * ontology is empty.
+	 */
 	@Test
-	public void shouldLoadOneOntology(){
-		fail("Not implemented");
+	public void shouldLoadNoOntology() {
+		final List<URL> emptyList = Collections.emptyList();
+		final AggregationEngine engine = new AggregationEngine(emptyList);
+		final ResultSet result = engine.execQuery("SELECT * WHERE {?x ?y ?x}");
+		assertEquals(0, result.getRowNumber());
 	}
 }
