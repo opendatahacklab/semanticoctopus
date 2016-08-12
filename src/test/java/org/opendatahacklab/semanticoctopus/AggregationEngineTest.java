021c8c87b132e0cc2116e469546032fce244c475
cristianolongo <cristianolongo@herbrand>
2016-08-12 17:00:50 +0200

    empty version of a test class

---
+++ src/test/java/org/opendatahacklab/semanticoctopus/AggregationEngineTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.opendatahacklab.semanticoctopus;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.opendatahacklab.semanticoctopus.AggregationEngine;
+
+/**
+ * Test cases for the {@link AggregationEngine}
+ * @author Cristiano Longo
+ *
+ */
+public class AggregationEngineTest {
+
+	@Test
+	public void shouldLoadOneOntology(){
+		fail("Not implemented");
+	}
+}
