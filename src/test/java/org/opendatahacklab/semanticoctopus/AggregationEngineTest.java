712a25aa6ce58abf12313b3c160cbf90b26a8017
cristianolongo <cristianolongo@herbrand>
2016-08-19 01:29:06 +0200

    #4 test about sparql queries against multiple ontologies

--- src/test/java/org/opendatahacklab/semanticoctopus/AggregationEngineTest.java
+++ src/test/java/org/opendatahacklab/semanticoctopus/AggregationEngineTest.java
@@ -3,14 +3,17 @@
  */
 package org.opendatahacklab.semanticoctopus;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.junit.Test;
 
@@ -21,30 +24,109 @@ import org.junit.Test;
  *
  */
 public class AggregationEngineTest {
+	private static final String RELATIVES_QUERY = "PREFIX testbed:<http://opendatahacklab.org/semanticoctopus/testbed/>\n"
+			+ "SELECT ?x ?y { ?x testbed:relative ?y }  ORDER BY ?x ?y";
 
-	public final URL ontologyA;
-	public final URL ontologyB;
-	public final URL ontologyC;
+	/**
+	 * An item of the result of a query about relatives
+	 * 
+	 * @author Cristiano Longo
+	 *
+	 */
+	class RelativePair {
+		final String x;
+		final String y;
+
+		RelativePair(final String x, final String y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	private final URL vocabulary;
+	private final URL ontologyA;
+	private final URL ontologyB;
+	private final URL ontologyC;
+	private final String individualA;
+	private final String individualB;
+	private final String individualC;
+	private final String individualD;
 
 	/**
 	 * @throws MalformedURLException
 	 * 
 	 */
 	public AggregationEngineTest() throws MalformedURLException {
-		ontologyA = new URL("http://opendatahacklab.github.io/semanticoctopus/testbed/A.owl");
-		ontologyB = new URL("http://opendatahacklab.github.io/semanticoctopus/testbed/B.owl");
-		ontologyC = new URL("http://opendatahacklab.github.io/semanticoctopus/testbed/C.owl");
+		vocabulary = new URL("http://opendatahacklab.org/semanticoctopus/testbed/V.owl");		
+		ontologyA = new URL("http://opendatahacklab.org/semanticoctopus/testbed/A.owl");
+		ontologyB = new URL("http://opendatahacklab.org/semanticoctopus/testbed/B.owl");
+		ontologyC = new URL("http://opendatahacklab.org/semanticoctopus/testbed/C.owl");
+		individualA = "http://opendatahacklab.org/semanticoctopus/testbed/a";
+		individualB = "http://opendatahacklab.org/semanticoctopus/testbed/b";
+		individualC = "http://opendatahacklab.org/semanticoctopus/testbed/c";
+		individualD = "http://opendatahacklab.org/semanticoctopus/testbed/d";
 	}
 
+	/**
+	 * Perform a query about relatives against the ontology obtained by
+	 * aggregating a set of ontologies.
+	 * 
+	 * @param ontologies URL of the ontologies to be aggregated
+	 * @param expected pairs expected to be returned as result of the relative query
+	 */
+	private void testRelatives(final List<URL> ontologies, final List<RelativePair> expected) {
+		final AggregationEngine engine = new AggregationEngine(ontologies);
+		final ResultSet actual = engine.execQuery(RELATIVES_QUERY);
+		final Iterator<RelativePair> expectedIt = expected.iterator();
+		while(expectedIt.hasNext()){
+			assertTrue("Too less pairs returned", actual.hasNext());
+			final RelativePair expectedPair = expectedIt.next();
+			final QuerySolution actualPair = actual.next();
+			assertEquals(expectedPair.x, actualPair.get("x"));
+			assertEquals(expectedPair.y, actualPair.get("y"));
+		}
+		assertFalse("Too much pairs returned", actual.hasNext());
+	}
+
+	/**
+	 * Conveniente method
+	 * 
+	 * @param ontologies
+	 * @param expected
+	 */
+	private void testRelatives(final URL[] ontologies, final RelativePair[] expected) {
+		testRelatives(Arrays.asList(ontologies), Arrays.asList(expected));
+	}
+	
 	/**
 	 * Check that if not ontology is provided as argument, the resulting
 	 * ontology is empty.
 	 */
 	@Test
 	public void shouldLoadNoOntology() {
-		final List<URL> emptyList = Collections.emptyList();
-		final AggregationEngine engine = new AggregationEngine(emptyList);
-		final ResultSet result = engine.execQuery("SELECT * WHERE {?x ?y ?x}");
-		assertEquals(0, result.getRowNumber());
+		testRelatives(new URL[0], new RelativePair[0]);
 	}
+
+	/**
+	 * Check loading a single ontology
+	 */
+	@Test
+	public void shouldLoadOneOntologyWithARelativeAssertion() {
+		final URL[] ontologies = {ontologyA};
+		final RelativePair[] expected = {new RelativePair(individualA, individualB)};
+		testRelatives(ontologies, expected);
+	}
+
+	/**
+	 * Check loading a multiple ontologies
+	 */
+	@Test
+	public void shouldLoadSeveralOntologiesWithSeveralRelativeAssertions() {
+		final URL[] ontologies = {ontologyA, ontologyB, ontologyC};
+		final RelativePair[] expected = {new RelativePair(individualA, individualB),
+				new RelativePair(individualB, individualC),
+				new RelativePair(individualC, individualD)};
+		testRelatives(ontologies, expected);
+	}
+
 }
