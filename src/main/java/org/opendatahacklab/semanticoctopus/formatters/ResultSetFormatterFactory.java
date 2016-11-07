78b7ec98a2858775a7971d9e17a0020a456bbc64
ool73 <fluoro@email.it>
2016-11-07 18:26:04 +0100

    #7 Factory of ResultSetFormatters and related tests modified

--- src/main/java/org/opendatahacklab/semanticoctopus/formatters/ResultSetFormatterFactory.java
+++ src/main/java/org/opendatahacklab/semanticoctopus/formatters/ResultSetFormatterFactory.java
@@ -1,9 +1,6 @@
 package org.opendatahacklab.semanticoctopus.formatters;
 
 import static com.hp.hpl.jena.sparql.resultset.ResultSetFormat.syntaxCSV;
-import static com.hp.hpl.jena.sparql.resultset.ResultSetFormat.syntaxJSON;
-import static com.hp.hpl.jena.sparql.resultset.ResultSetFormat.syntaxRDF_XML;
-import static com.hp.hpl.jena.sparql.resultset.ResultSetFormat.syntaxTSV;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
@@ -15,7 +12,7 @@ import com.hp.hpl.jena.sparql.resultset.ResultSetFormat;
 
 /**
  * A factory of {@link ResultSetFormatter}s based on mime-type
- * 
+ *
  * @author OOL
  */
 public class ResultSetFormatterFactory {
@@ -27,11 +24,11 @@ public class ResultSetFormatterFactory {
 
 		{
 			put("text/csv",syntaxCSV);
-			put("text/tab-separated-values",syntaxTSV);
-			put("application/sparql-results+json",syntaxJSON);
-			put("application/json",syntaxJSON);
-			put("application/sparql-results+xml",syntaxRDF_XML);
-			put("application/xml",syntaxRDF_XML);
+			// put("text/tab-separated-values",syntaxTSV);
+			// put("application/sparql-results+json",syntaxJSON);
+			// put("application/json",syntaxJSON);
+			// put("application/sparql-results+xml",syntaxRDF_XML);
+			// put("application/xml",syntaxRDF_XML);
 		}
 	};
 
@@ -41,11 +38,11 @@ public class ResultSetFormatterFactory {
 	 * Accepted mime-types are: <code>text/csv</code>, <code>text/tab-separated-values<code></code>,
 	 * <code>application/sparql-results+json</code>,<code>application/json</code>,
 	 * <code>application/sparql-results+xml</code>,<code>application/xml</code>
-	 * 
+	 *
 	 * @param mimeType
-	 * 
+	 *
 	 * @return
-	 * 
+	 *
 	 * @throws IllegalMimeTypeException
 	 *             when mime-type is not accepted in the context of SPARQL SELECT queries
 	 */
@@ -56,19 +53,43 @@ public class ResultSetFormatterFactory {
 		// return resultSetFormatter;
 		// else
 		// throw new IllegalMimeTypeException(mimeType);
-		throw new RuntimeException("Unimplemented yet!");
+
+		// throw new RuntimeException("Unimplemented yet!");
+
+		final ResultSetFormat resultSetFormat = formats.get(mimeType);
+
+		if (resultSetFormat != null)
+			return new ResultSetFormatter() {
+
+			@Override
+			public String getMimeType() {
+				return mimeType;
+			}
+
+			@Override
+			public String format(final ResultSet resultSet) {
+				if (resultSetFormat == syntaxCSV) {
+					final OutputStream outputStream = new ByteArrayOutputStream();
+						com.hp.hpl.jena.query.ResultSetFormatter.outputAsCSV(outputStream, resultSet);
+						return outputStream.toString();
+				}
+				return formatResult(resultSet, resultSetFormat);
+			}
+		};
+		else
+			throw new IllegalMimeTypeException(mimeType);
 	}
 
 	/**
 	 * Formats specified result set according to a certain syntax
-	 * 
+	 *
 	 * @param resultSet
 	 * @param format
 	 *            Format related to syntax
-	 * 
+	 *
 	 * @return A string representing the result set in requested format
 	 */
-	private String formatResult(final ResultSet resultSet, final ResultSetFormat format) {
+	private static String formatResult(final ResultSet resultSet, final ResultSetFormat format) {
 		final OutputStream outputStream = new ByteArrayOutputStream();
 		com.hp.hpl.jena.query.ResultSetFormatter.output(outputStream, resultSet, format);
 		final String formattedResultSet = outputStream.toString();
--- src/test/java/org/opendatahacklab/semanticoctopus/formatters/ResultSetFormatterFactoryTest.java
+++ src/test/java/org/opendatahacklab/semanticoctopus/formatters/ResultSetFormatterFactoryTest.java
@@ -2,15 +2,21 @@ package org.opendatahacklab.semanticoctopus.formatters;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
 
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.QuerySolutionMap;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
 
 /**
  * Test class of {@link ResultSetFormatterFactory}
- * 
+ *
  * @author OOL
  */
 @Ignore
@@ -25,40 +31,77 @@ public class ResultSetFormatterFactoryTest {
 	private static final String XML_MT = "application/xml";
 	private static final String TEXT_MT = "text/plain"; // Unaccepted
 
+	private static final String SUBJECT = "http://eu.ool.ex/resources#ools";
+	private static final String PREDICATE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+	private static final String OBJECT = "http://eu.ool.ex/resources#Person";
+	private static final String SUBJ_VAR = "s";
+	private static final String PRED_VAR = "p";
+	private static final String OBJ_VAR = "o";
+
+	private static final String COMMA = ",";
+	private static final String TAB = "\t";
+	private static final String A_CAPO = "\n";
+	private static final String CSV_REPR = SUBJ_VAR + COMMA + PRED_VAR + COMMA + OBJ_VAR + A_CAPO +
+					SUBJECT + COMMA + PREDICATE + COMMA + OBJECT + A_CAPO;
+
 	private ResultSet createResultSet() {
-		return mock(ResultSet.class); // TODO
+		final QuerySolution solution = createQuerySolution();
+
+		final ResultSet resultSet = mock(ResultSet.class);
+
+		when(resultSet.hasNext()).thenReturn(true, false);
+		when(resultSet.next()).thenReturn(solution, (QuerySolution) null);
+		// when(resultSet.nextBinding()).thenReturn(new Binding(SUBJ_VAR, SUBJECT), new Binding(PRED_VAR, PREDICATE),
+		// new Binding(OBJ_VAR, OBJECT), (Binding) null);
+		when(resultSet.nextSolution()).thenReturn(solution, (QuerySolution) null);
+		when(resultSet.getResultVars()).thenReturn(Arrays.asList(SUBJ_VAR, PRED_VAR, OBJ_VAR));
+		when(resultSet.getRowNumber()).thenReturn(1);
+
+		return resultSet;
+	}
+
+	/**
+	 * @return
+	 */
+	private QuerySolution createQuerySolution() {
+		final QuerySolutionMap solution = new QuerySolutionMap();
+		solution.add(SUBJ_VAR, new ResourceImpl(SUBJECT));
+		solution.add(PRED_VAR, new ResourceImpl(PREDICATE));
+		solution.add(OBJ_VAR, new ResourceImpl(OBJECT));
+
+		return solution;
 	}
 
 	/**
 	 * @throws IllegalMimeTypeException
-	 * 
+	 *
 	 */
 	@Test
 	public void shouldBuildCSVFormatter() throws IllegalMimeTypeException {
-		checkFormatter(CSV_MT, createResultSet(), ""); // TODO
+		checkFormatter(CSV_MT, createResultSet(), CSV_REPR);
 	}
 
 	/**
 	 * @throws IllegalMimeTypeException
-	 * 
+	 *
 	 */
 	@Test
 	public void shouldBuildTSVFormatter() throws IllegalMimeTypeException {
 		checkFormatter(TSV_MT, createResultSet(), ""); // TODO
 	}
+
 	/**
 	 * @throws IllegalMimeTypeException
-	 * 
+	 *
 	 */
 	@Test
 	public void shouldBuildSPARQLJSONFormatter() throws IllegalMimeTypeException {
 		checkFormatter(SPARQL_JSON_MT, createResultSet(), ""); // TODO
 	}
 
-
 	/**
 	 * @throws IllegalMimeTypeException
-	 * 
+	 *
 	 */
 	@Test
 	public void shouldBuildJSONFormatter() throws IllegalMimeTypeException {
@@ -67,7 +110,7 @@ public class ResultSetFormatterFactoryTest {
 
 	/**
 	 * @throws IllegalMimeTypeException
-	 * 
+	 *
 	 */
 	@Test
 	public void shouldBuildSPARQLXMLFormatter() throws IllegalMimeTypeException {
@@ -76,7 +119,7 @@ public class ResultSetFormatterFactoryTest {
 
 	/**
 	 * @throws IllegalMimeTypeException
-	 * 
+	 *
 	 */
 	@Test
 	public void shouldBuildXMLFormatter() throws IllegalMimeTypeException {
@@ -84,24 +127,23 @@ public class ResultSetFormatterFactoryTest {
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	@Test(expected = IllegalMimeTypeException.class)
 	public void shouldRaiseExceptionOnIllegalMimeType() throws IllegalMimeTypeException {
 		ResultSetFormatterFactory.createFormatter(TEXT_MT);
 	}
 
-
 	/**
 	 * Checks that a certain mime-type corresponds to specified formatter
-	 * 
+	 *
 	 * @param mimeType
 	 * @param formatter
-	 * 
+	 *
 	 * @throws IllegalMimeTypeException
 	 */
 	private void checkFormatter(final String mimeType, final ResultSet resultSet, final String expectedFormattedResult)
-			throws IllegalMimeTypeException {
+					throws IllegalMimeTypeException {
 		final ResultSetFormatter testSubject = ResultSetFormatterFactory.createFormatter(mimeType);
 		assertEquals("Wrong mime type", mimeType, testSubject.getMimeType());
 
